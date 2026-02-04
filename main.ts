8a11c534779d22ded6490954ecabe451e9636202
Savannah Young <139026125+LinuxLoser137@users.noreply.github.com>
2026-02-04 12:59:09 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+/**
+ * ****Create a simple but complete game that proves you understand the basics.****
+ * 
+ * --You must create at least 3 sprites
+ * 
+ * Example: a player, an enemy, and a collectible
+ * 
+ * ---You must use at least 1 loop
+ * 
+ * Example: a *forever loop or a *repeat loop
+ * 
+ * ----At least 2 sprites must do something
+ * 
+ * Example: move, follow, collide, or disappear
+ * 
+ * -----You must use at least 1 logic statement
+ * 
+ * Example: *if… then
+ * 
+ * ------Your game must run without crashing
+ * 
+ * No error messages or frozen screens
+ */
